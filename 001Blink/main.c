384838139e4a53ba3bf83647d20a4fb767b6e910
Mestan Ünsal TEKİŞ <83555018+tekis90@users.noreply.github.com>
2025-07-16 21:07:45 +0300

    Update main.c

--- 001Blink/main.c
+++ 001Blink/main.c
@@ -2,6 +2,7 @@
 
 const uint LED_PIN = 25;
 
+const uint TIME_MS = 500;
 int main() {
 
     // The pin named LED_PIN is initialized
@@ -13,10 +14,10 @@ int main() {
     while (true) {
         // Set the pin HIGH (turn LED on)
         gpio_put(LED_PIN, 1);
-        sleep_ms(200);
+        sleep_ms(TIME_MS);
 
         // Set the pin LOW (turn LED off)
         gpio_put(LED_PIN, 0);
-        sleep_ms(200);
+        sleep_ms(TIME_MS);
     }
 }
